4a00fd3058c2f7b3f6712b923d647338fd8d167a
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-08-02 20:01:50 -0600

    Set up database and reseed via json-server

---
+++ server/db-actions.ts
@@ -0,0 +1,13 @@
+/*
+    * Quotes
+        - Get all quotes
+        - Get quotes by category
+    * Users
+        - Get all users
+        - Get user by email
+        - Add new user
+    * Favorites
+        - Get favorites by user
+        - Add new favorite
+        - Delete favorite
+*/
