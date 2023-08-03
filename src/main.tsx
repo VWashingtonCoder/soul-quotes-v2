4e1a5bc34a8a4053542f5c98ab14a6e38c1ab76d
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-08-03 16:05:07 -0600

    Route providers into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QuotesProvider } from "../backend/providers/QuotesProvider.tsx";
+import { UsersProvider } from "../backend/providers/UsersProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QuotesProvider>
+      <UsersProvider>
+        <App />
+      </UsersProvider>
+    </QuotesProvider>
+  </React.StrictMode>
+);
