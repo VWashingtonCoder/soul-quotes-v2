20d0c8b72383b3d80287c9476e3d361ed5cee26b
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-08-02 20:19:22 -0600

    Set up necessary api-actions

---
+++ backend/db-actions.ts
@@ -0,0 +1,56 @@
+import { Favorite, User } from '../types';
+const BASE_URL = 'http://localhost:3000';
+
+export const getAllQuotes = async () => {
+    const response = await fetch(`${BASE_URL}/quotes`);
+    return await response.json();
+}
+
+export const getQuotesByCategory = async (category: string) => {
+    const response = await fetch(`${BASE_URL}/quotes?category=${category}`);
+    return await response.json();
+}
+
+export const getAllUsers = async () => {
+    const response = await fetch(`${BASE_URL}/users`);
+    return await response.json();
+}
+
+    const response = await fetch(`${BASE_URL}/usersexport const getUserByEmail = async (email: string) => {
+?email=${email}`);
+    return await response.json();
+}
+
+export const addUser = async (user: User) => {
+    const response = await fetch(`${BASE_URL}/users`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    });
+    return await response.json();
+}
+
+export const getFavoritesByUser = async (userId: string) => {
+    const response = await fetch(`${BASE_URL}/favorites?userId=${userId}`);
+    return await response.json();
+}
+
+export const addFavorite = async (favorite: Favorite) => {
+    const response = await fetch(`${BASE_URL}/favorites`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(favorite)
+    });
+    return await response.json();
+}
+
+export const deleteFavorite = async (id: number) => {
+    const response = await fetch(`${BASE_URL}/favorites/${id}`, {
+        method: 'DELETE'
+    });
+    return await response.json();
+}
